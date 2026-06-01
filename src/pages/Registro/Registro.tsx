ec10889ebe760d8b6ca3abbf3855e154a76f6c16
Camila Quinones <camiqg2905@gmail.com>
2026-06-01 09:29:38 -0500

    otro arreglo en registro

--- src/pages/Registro/Registro.tsx
+++ src/pages/Registro/Registro.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { useNavigate, Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import "./Registro.css";
 import flor from "../../assets/flor.png";
 
